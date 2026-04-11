6daa76fcf019ae95eb54036b715d19351e2d40a6
Afonso Sousa <afonso.rocha@outlook.com>
2026-04-11 16:45:34 +0100

    rigorous parsing method

--- src/parsing.c
+++ src/parsing.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include "obj.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 256
 
@@ -47,28 +48,7 @@ char *get_next_line(int fd)
     return (line);
 }
 
-void	free_until_null(char ***str_v)
-{
-	size_t	i;
 
-	i = 0;
-	if (!*str_v)
-		return ;
-	while ((*str_v)[i])
-		free((*str_v)[i++]);
-	free(*str_v);
-	*str_v = NULL;
-}
-
-static size_t ft_count (char **str)
-{
-    size_t count;
-
-    count = 0;
-    while (str && *str++)
-        count++;
-    return (count);   
-}
 
 static size_t  magnitude(int n)
 {
@@ -86,64 +66,154 @@ static size_t  magnitude(int n)
 }
 
 
-double  ft_atod(const char *nbr)
+
+
+
+bool    ft_isspace(char c)
+{
+    return (c == ' ' || (c >= 9 && c <= 13));
+}
+
+
+bool    skip(char **s, bool (*predicate)(char))
+{
+    bool ret;
+
+    ret = false;
+    while (**s && predicate(**s))
+    {
+        (*s)++;
+        ret = true;
+    }
+    return (ret);
+}
+
+bool  parse_double(char **line, double *d)
 {
     double integer;
     double decimal;
 
-    integer = ft_atoi(nbr);
-    nbr += magnitude(integer) + 1;
-    // o wallace tem medo de dois pontos..
-    while (*nbr && !ft_isdigit(*nbr))
-        nbr++;
-    decimal = ft_atoi(nbr);
-    return ((double) integer 
-        + (((integer < 0) * -1) + integer >= 0) 
-        * (decimal / pow(10.0, (double) magnitude(decimal))));
+    if (!ft_isdigit(**line) && **line != '-' && **line != '+')
+        return (false);
+    integer = ft_atoi((const char *)*line);
+    *line += magnitude(integer) + (integer < 0 || **line == '+' || **line == '-');
+    if (**line != '.')
+    {
+        *d = (double)integer;
+        return (true);
+    }
+    (*line)++;
+    if (!ft_isdigit(**line))
+        return (false);
+    decimal = ft_atoi((const char *) *line);
+    *d = (double) integer 
+        + (((integer < 0) * -1) + (integer >= 0)) 
+        * (decimal / pow(10.0, (double) magnitude(decimal)));
+    *line += magnitude(decimal);
+    return (true);
 }
 
-//TODO: check coords FULL integrity
-// sp 0.0,0.0,20.6 12.6 10,0,255
-void    parse_sphere(char **params, t_obj *obj)
+//maybe move the division upstream
+bool    parse_vec3_uchar(char **line, t_vec3    *vec)
 {
-    char    **coords;
-    char    **colors;
-    
-    obj->type = ERR;
-    if (ft_count(params) != 3)
-        return ;
-    coords = ft_split(params[0], ',');
-   if (ft_count(coords) != 3)
-        return (free(coords));
-    obj->pos = vec3(
-        ft_atod(coords[0]),
-        ft_atod(coords[1]),
-        ft_atod(coords[2])
-    ); 
-    obj->data.sphere.radius = ft_atod(params[1]);
-    colors = ft_split(params[2], ',');
-    if (ft_count(colors) != 3)
-        return (free(coords), free(colors)); 
-    obj->color = vec3(
-        ft_atod(colors[0]) / 255.0,
-        ft_atod(colors[1]) / 255.0,
-        ft_atod(colors[2]) / 255.0
-    );
+    unsigned char x;
+    unsigned char y;
+    unsigned char z;
+
+    if (!ft_isdigit(**line))
+        return (false);
+    x = ft_atoi((const char *)*line);
+    *line += magnitude(x);
+    if (**line != ',')
+        return (false);
+    (*line)++;
+    if (!ft_isdigit(**line))
+        return (false);
+    y = ft_atoi((const char *)*line);
+    *line += magnitude(y);
+    if (**line != ',')
+        return (false);
+    (*line)++;
+    if (!ft_isdigit(**line))
+        return (false);
+    z = ft_atoi((const char *)*line);
+    *line += magnitude(z);
+    *vec = vec3(x / 255.0, y / 255.0, z / 255.0);
+    return (true);
+}
+
+bool    parse_vec3_double(char **line, t_vec3   *vec)
+{
+    double  x;
+    double  y;
+    double  z;
+
+    if (!parse_double(line, &x) || **line != ',')
+        return (false);
+    (*line)++;
+    if (!parse_double(line, &y) || **line != ',')
+        return (false);
+    (*line)++;
+    if (!parse_double(line, &z))
+        return (false);
+    *vec = vec3(x, y, z);
+    return (true);
+}
+
+bool    parse_sphere_s(char **line, t_obj *obj)
+{
+    *line += 2;
+    if (!skip(line, ft_isspace))
+        return (false);
+    if (!parse_vec3_double(line, &obj->pos))
+        return (false);
+    if (!skip(line, ft_isspace))
+        return (false);
+    if (!parse_double(line, &obj->data.sphere.radius))
+        return (false);
+    if (!skip(line, ft_isspace))
+        return (false);
+    if (!parse_vec3_uchar(line, &obj->color))
+        return (false);
     obj->type = OBJ_SPHERE;
+    return (true);
 }
 
-// sets obj-type to error if the parsing fails
-t_obj   *parse_line(char *line, t_obj *obj)
+bool    parse_line_s(char **line, t_obj *obj)
 {
-    char **params;
+    skip(line, ft_isspace);
+    if (**line == '\0' || **line == '\n')
+    {
+        obj->type = ERR; // or another type representing "empty line" to safely skip it
+        return (true);
+    }
+    if (ft_strncmp(*line, "sp", 2) == 0)
+        return (parse_sphere_s(line, obj));
+    return (false);
+}
 
-    params = ft_split(line, ' ');
-    if (!params)
-        return (NULL);    
-    if (ft_strncmp(params[0], "sp", 2) == 0)
-        parse_sphere(&params[1], obj);
-    free_until_null(&params);
-    return (obj);
+void    syntax_error(const char *file, int line, const char *start, const char *current)
+{
+    int offset;
+    int i;
+    int len;
+
+    offset = current - start;
+    printf("%s:%d:%d: error: syntax error\n", file, line, offset + 1);
+    printf("%s", start);
+    len = ft_strlen(start);
+    if (len == 0 || start[len - 1] != '\n')
+        printf("\n");
+    i = 0;
+    while (i < offset)
+    {
+        if (start[i] == '\t')
+            printf("\t");
+        else
+            printf(" ");
+        i++;
+    }
+    printf("^\n");
 }
 
 //maybe detect errors here, also error handling
@@ -152,13 +222,18 @@ t_world *parse_file(t_world *wrld, char *path)
     int     fd;
     t_obj   obj;
     size_t  i;
+    char    *line;
+    char    *current;
 
     wrld->size = 0;
     fd = open(path, O_RDONLY);
     if (fd < 0)
         return (NULL);
-    while (get_next_line(fd))
+    while ((line = get_next_line(fd)))
+    {
         wrld->size++;
+        free(line);
+    }
     close(fd);
     fd = open(path, O_RDONLY);
     if (fd < 0)
@@ -167,10 +242,16 @@ t_world *parse_file(t_world *wrld, char *path)
     i = 0;
     while (i < wrld->size)
     {
-        parse_line(get_next_line(fd), &obj);
-        if (obj.type == ERR)
-            break ;
+        line = get_next_line(fd);
+        current = line;
+        if (!parse_line_s(&current, &obj))
+        {
+            syntax_error(path, i + 1, line, current);
+            free(line);
+            return (NULL);
+        }
         wrld->map[i++] = obj;
+        free(line);
     }
     return (wrld);
 }
