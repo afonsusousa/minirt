5b6890fd99f353ee27c6c00e10cb22bc316646fd
wlucas-f <wallacelfgoncalves@gmail.com>
2026-04-16 19:39:11 +0100

    now the actual interval was added

---
+++ includes/interval.h
@@ -0,0 +1,28 @@
+//
+// Created by wlucas-f on 4/16/26.
+//
+
+#ifndef MINIRT_INTERVAL_H
+#define MINIRT_INTERVAL_H
+
+typedef struct s_interval
+{
+    double min;
+    double max;
+} t_interval;
+
+inline double interval_size(t_interval interval)
+{
+    return (interval.max - interval.min);
+}
+
+inline bool contains(t_interval interval, double value)
+{
+    return (interval.min <= value && value <= interval.max);
+}
+
+inline bool surrounds(t_interval interval, double value)
+{
+    return (interval.min < value && value < interval.max);
+}
+#endif //MINIRT_INTERVAL_H
